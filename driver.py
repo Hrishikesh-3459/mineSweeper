2c52b62627e7cef0b6a94031dd6ecf6df45a4a49
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-05-13 15:27:48 +0530

    Added recursive function, game running.

--- driver.py
+++ driver.py
@@ -1,19 +1,29 @@
+# Code starts at line 110
+
 from random import randint
 
-B_ROW = 10 # Defining the number of rows for a beginner level
-B_COL = 10 # Defining the number of columns for a beginner level
-B_NO_OF_BOMBS_BOMBS = 10 # Defining the number of bombs for a beginner level
+ROW = 10 # Defining the number of rows for a beginner level
+COL = 10 # Defining the number of columns for a beginner level
+NO_OF_BOMBS = 10 # Defining the number of bombs for a beginner level
+FULL_LINE = 190 # Constant for design purposes
 
-grid = [[0] * B_COL for j in range(B_ROW)] # Creating a grid 
+grid = [['0'] * COL for j in range(ROW)] # Creating a grid 
+board = [["."] * COL for j in range(ROW)] # Creating a board, that the user will see
 
 
 # Function to generate bombs at random locations in the grid
 def gen_bombs():
-    for i in range(B_NO_OF_BOMBS_BOMBS+1):
-        location = {}
-        location['row'] = randint(0, B_ROW-1)
-        location['column'] = randint(0, B_COL-1)
-        grid[location['row']][location['column']] = '*' # '*' Indicates a bomb in the grid
+    ctr = 0
+    while True:
+        row = randint(0, ROW-1)
+        col = randint(0, COL-1)
+        if (grid[row][col] != '*'):
+            grid[row][col] = '*' # '*' Indicates a bomb in the grid
+            ctr += 1
+        if(ctr == NO_OF_BOMBS):
+            break
+        
+
 
 
 # Function to print the grid
@@ -23,42 +33,93 @@ def print_grid():
         print("   |", i, end="")
     print("   |")
     k = 0
-    for i in range(B_ROW):
+    for i in range(ROW):
         print(k, end="")
-        for j in range(B_COL):
+        for j in range(COL):
             print("  |  " + str(grid[i][j]), end="")
         print("  |")
         k += 1
 
+# Function to print the board that is visible to the user
+def print_board():
+    BORDER = 64
+    POS_INDICATOR = 10
+    for i in range(FULL_LINE):
+        print("-", end="")
+    print()
+    for i in range(BORDER):
+        print("_", end="")
+    print()
+    k = 0
+    for i in range(POS_INDICATOR):
+        print("   |", i, end="")
+    print("   |")
+    for i in range(ROW):
+        print(k, end="")
+        for j in range(COL):
+            print("  |  " + str(board[i][j]), end="")
+        print("  |")
+        k += 1
+    for i in range(BORDER):
+        print("_", end="")
+    print()
+
 
 # function to calculate the number of bombs in the vicinity of the given cell whose coordianates (row, col) are passed
-def check_count(row, col):
-    # Haven't yet figured out the logic for corner cases, it's a work in progress :)
-    if (row == 0 or row == 9 or col == 0 or col == 9):
-        return '#' 
+def check_count(row, col): 
     count = 0
-    k = -1
-    for i in range(3): # Used 3 to iterate over the cells above and below the current cell
-        r = -1
-        for j in range(3): # Used 3 to iterate over the cells on the left and right of the current cell
-            if (grid[row + k][col + r] == '*'):
+    r = -1 # Row constant, to iterate around a given cell 
+    while(r < 2): 
+        c = -1 # Column constant, to iterate around a given cell
+        while(c < 2): 
+            if (row + r not in range(0,ROW) or col + c not in range(0,COL)): # Skips over boundry cases
+                pass
+            elif (grid[row + r][col + c] == '*'): # counts the number if bombs around the given cell
                 count += 1
-            r += 1
-        k += 1
+            c += 1
+        r += 1
     return (count) # The number of bombs are returned
 
 # This function iterates over every cell in the 2D list 'grid' and calculates the number of bombs near it
 def get_index():
-    for i in range(B_ROW):
-        for j in range(B_COL):
-            if (grid[i][j] == '*'):
-                continue
-            grid[i][j] = check_count(i,j)
+    for i in range(ROW):
+        for j in range(COL):
+            if (grid[i][j] != '*'):
+                grid[i][j] = str(check_count(i,j))
 
+            
+# Recursive function, to print the value at user's input
+def open_grid(row, col):
+    board[row][col] = grid[row][col]
+    if(board[row][col] == '0'):
+        r = -1
+        while(r < 2):  
+            c = -1
+            while(c < 2):
+                if (row + r not in range(0,ROW) or col + c not in range(0,COL)):
+                    pass
+                elif (board[row+r][col+c] == '.'):
+                    if (grid[row + r][col + c] == '0'):
+                        open_grid(row + r, col + c)
+                    board[row+r][col+c] = grid[row+r][col+c]
+                c += 1
+            r += 1
 
-
-
+    
 # Calling the functions
 gen_bombs()
 get_index()
-print_grid()
+print_grid() # This is just for beta testing, so that we know where the bombs are
+print_board()
+
+while True:
+    row = int(input("Enter the row coordinate: "))
+    col = int(input("Enter the col coordinate: "))
+    open_grid(row,col)
+    print_board()
+
+    if (grid[row][col] == '*'):
+        print_grid() # Have to change is so as to print only the bombs
+        print("Bomb Found")
+        print("Game Over")
+        break
